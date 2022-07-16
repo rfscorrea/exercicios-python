7b2393ea507f0130804a35b5196656309dcb2821
Rafael Corrêa <orafasouzacorrea@gmail.com>
2022-07-16 03:45:11 -0300

    ex 111

---
+++ ex111.py
@@ -0,0 +1,7 @@
+"""Exercício Python 111: Crie um pacote chamado utilidadesCeV
+que tenha dois módulos internos chamados moeda e dado. Transfira
+todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro
+ e mantenha tudo funcionando."""
+
+from utilidadesCeV import moeda
+print(f'{moeda.resumo(500, 10, 15)}')
