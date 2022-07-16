097039f2c48976c5fef765987b735442c071b995
Rafael Corrêa <orafasouzacorrea@gmail.com>
2022-07-16 03:45:15 -0300

    ex 112

---
+++ ex112.py
@@ -0,0 +1,13 @@
+"""Exercício Python 112: Dentro do pacote utilidadesCeV
+que criamos no desafio 111, temos um módulo chamado dado.
+Crie uma função chamada leiaDinheiro() que seja capaz de funcionar
+como a função input(), mas com uma validação de dados para aceitar
+apenas valores que seja monetários."""
+
+from utilidadesCeV import dado
+from utilidadesCeV import moeda
+
+
+a = dado.leiadinheiro('Digite: ')
+print(f'{moeda.resumo(a, 10, 10)}')
+
